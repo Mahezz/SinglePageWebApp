5679cb1da1a80bb3471f112d04a4e33c1948a8e9
angel torres <angeltorres98@hotmail.com>
2023-08-31 13:30:39 +0200

    31/08/2023: SinglePageApp config - Añadir componentes, bootstrap, imports.

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+})
+export class AboutComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+}
