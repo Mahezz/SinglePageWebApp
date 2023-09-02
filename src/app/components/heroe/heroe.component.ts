876d2db302e401aa7ea8e822222c64e0e9c6e4dd
angel torres <angeltorres98@hotmail.com>
2023-09-02 23:23:50 +0200

    02/09/2023-Tarea: Componente y pagina para el heroe y sus detalles. NgIf, RouterLink, Interpolation.

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -7,12 +7,23 @@ import { HeroesService } from 'src/app/services/heroes.service';
   templateUrl: './heroe.component.html'
 })
 export class HeroeComponent {
-
+    heroeImg: string = "none"
     heroe:any = {}
 
     constructor( private activatedRoute: ActivatedRoute, private _heroesService: HeroesService){
       this.activatedRoute.params.subscribe(params=> {
         this.heroe = this._heroesService.getHeroe(params['id'])
+        console.log(this.heroe)
       })
+    }
+
+    getHeroeImg(heroeCompany:string){
+      if(heroeCompany="DC"){
+        this.heroeImg = "DCLogo.png"
+      }else if(heroeCompany="Marvel"){
+        this.heroeImg = "marvelLogo.png"
+      }
+
+      console.log(this.heroeImg)
     }
 }
